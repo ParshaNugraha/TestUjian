96117f5c80a13c66a07bb25f49c1dc72d23383c2
ParshaNugraha <pramudyashaka27@gmail.com>
2023-11-22 17:08:58 +0700

    menambahkan perulangan

--- app/src/main/java/com/example/ujianandroid/ListActivity.java
+++ app/src/main/java/com/example/ujianandroid/ListActivity.java
@@ -2,9 +2,7 @@ package com.example.ujianandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -17,17 +15,22 @@ public class ListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
-        ListView lvNama = (ListView) findViewById(R.id.lvNama);
+        ListView lvNama = findViewById(R.id.lvNama);
 
         ArrayList<String> daftar_nama = getIntent().getExtras().getStringArrayList("daftar_nama");
 
-        if(daftar_nama.isEmpty()){
+        if (daftar_nama.isEmpty()) {
             daftar_nama.add("data masih kosong");
         }
 
-        ArrayAdapter<String> ad_nama = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, daftar_nama);
+        // Repeat the names up to 20 times
+        ArrayList<String> repeatedNames = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            repeatedNames.addAll(daftar_nama);
+        }
+
+        ArrayAdapter<String> ad_nama = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, repeatedNames);
 
         lvNama.setAdapter(ad_nama);
-
     }
 }
